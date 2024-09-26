8cf8604209d2c0e38776946e25757fe78d73e40a
linzy <ccamy0915@naver.com>
2024-09-26 11:28:27 +0900

    refactor

--- src/main/java/kr/co/shinhands/edu/aidev/App.java
+++ src/main/java/kr/co/shinhands/edu/aidev/App.java
@@ -4,12 +4,14 @@ package kr.co.shinhands.edu.aidev;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        sayHello("Hello World!");
+    }
+
+    private static void sayHello(String string) {
         for (int i = 0; i < 10; i++) {
-            System.out.println( "Hello World!" );
+            System.out.println(string);
         }
     }
 }
